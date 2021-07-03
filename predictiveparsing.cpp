cf4c3173602ab5f5c64823efb7de2a147e416746
Amruta Jandhyala <amrutajandhyala@outlook.com>
2021-07-03 16:02:53 +0530

    Still doing 5th question, but need Visual Studio C++ debugger

--- predictiveparsing.cpp
+++ predictiveparsing.cpp
@@ -1,32 +1,88 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
+#include<string>
+#include<cctype.h>
+#include<fstream>
+#include<sstream>
 using namespace std;
 
+struct NonTerminalDetails {
+    char lhs;
+    string rhs;
+    vector<char> first;
+    vector<char> follow;
+}nt[30];
 
-vector<string> outputlhslist;
-void printOutput(map<string, vector<string>> output){
-    for(size_t i=0; i<outputlhslist.size(); i++){
-        for(size_t j=0; j<output[outputlhslist[i]].size(); j++){
-            cout << outputlhslist[i] << " -> " << output[outputlhslist[i]][j] << endl;
-        }
+void printProductions(int productioncount) {
+    for (size_t i = 0; i < productioncount; i++)
+    {
+        cout << nt[i].lhs << " -> " << nt[i].rhs << endl;
     }
+
 }
 
-auto addTerminals(vector<char> terminals, string rhs){
-    int flag=0;
+
+
+auto addTerminals(vector<char> terminals, string rhs) {
+    int flag = 0;
     for (size_t i = 0; i < rhs.length(); i++)
     {
-        if(!isupper(rhs[i])){
-            for (size_t j = 0; j < terminals.size(); j++){
-                if(rhs[i]==terminals[j]){
-                    flag=1;
+        if (!isupper(rhs[i])) {
+            for (size_t j = 0; j < terminals.size(); j++) {
+                if (rhs[i] == terminals[j]) {
+                    flag = 1;
                 }
             }
-            if(flag==0){
+            if (flag == 0) {
                 terminals.push_back(rhs[i]);
             }
-        } 
+        }
     }
     return terminals;
+}
+
+void firstx(char X) {
+
+}
+
+
+auto first(string alpha) {
+    vector<char> firstterminals;
+    firstterminals = addTerminals(firstterminals, alpha);
+    for (size_t i = 0; i < alpha.length(); i++)
+    {
+        if (isupper(alpha[i])) {
+            for (size_t i = 0; i < nt.size(); i++)
+            {
+                if (nt.lhs == alpha[i]) {
+                    for (size_t i = 0; i < nt.first.size(); i++)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+    return firstterminals;
+}
+
+auto constructPredictiveParsingTable(string parsingtable[100][100]) {
+
+    return parsingtable;
+}
+
+
+
+string eliminateSpaces(string line) {
+    stringstream ss;
+    ss << line;
+    string word;
+    line = "";
+    while (!ss.eof()) {
+        ss >> word;
+        line = line + word;
+    }
+    return line;
 }
 
 int main(int argc, char* argv[]){
@@ -44,31 +100,25 @@ int main(int argc, char* argv[]){
         return 0;
     }
     string line;
-    // A map to store the productions
-    map <string, vector<string>> productions;
+    int productioncount=0;
     // A vector to create the ordered list of LHS
-    vector<string> lhslist;
+    vector<char> lhslist;
+    // A vector to create the list of all terminals, '@' is included
     vector<char> terminals;
+
     while(getline(filebuf,line)){
         // Ignore comments in input file, they start with '#'
         if(line[0]=='#'){
             continue;
         }
         // Eliminate spaces in each line
-        stringstream ss;
-        ss << line;
-        string word;
-        line = "";
-        while (!ss.eof()) {
-            ss >> word;
-            line = line + word;
-        }
+        line = eliminateSpaces(line);
         if(line.empty()){
             continue;
         }
-        //cout << line << endl;
+        // cout << line << endl;
         // Get LHS from line and add to list
-        string lhs = string(1, line[0]);
+        char lhs = line[0];
         lhslist.push_back(lhs);
         // Remove the "S->" or "A->" part of the line
         line.erase(0,3);
@@ -81,22 +131,19 @@ int main(int argc, char* argv[]){
                 break;
             }
             string rhs;
+            nt[productioncount].lhs = lhs;
             // RHS is from beginning till "|"
             rhs = line.substr(0,pipepos);
-            // Add the production to the map
-            productions[lhs].push_back(rhs);
+            nt[productioncount].rhs = rhs;
+            productioncount++;
             terminals = addTerminals(terminals, rhs);
             // Remove the first RHS part including the pipe "|" from the line
             line.erase(0,pipepos+1);
-        }
-        productions[lhs].push_back(line);
-        cout << "The productions are: " << endl;
-        for(size_t i=0; i<lhslist.size(); i++){
-            for(size_t j=0; j<productions[lhslist[i]].size(); j++){
-                cout << lhslist[i] << " -> " << productions[lhslist[i]][j] << endl;
-            }
-        }
+        } 
     } 
+    printProductions(productioncount);
+    string parsingtable[100][100];
+    parsingtable = constructPredictiveParsingTable(parsingtable);
     string input;
     cout << "Enter any input string: ";
     cin >> input;
